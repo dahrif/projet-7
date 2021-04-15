ea02c52262afb9cf8f200edd7b35713e5ee8e885
dahrif <dahri.fatima.dev@gmail.com>
2021-04-15 13:39:03 +0000

    realisation

--- realisation/CalculatriceBLO.js
+++ realisation/CalculatriceBLO.js
@@ -1,65 +1,39 @@
 // Métier
 class CalculatriceBLO {
+constructor(x,y,solution,operation){
+    this.x=x;
+    this.y=y;
+    this.solution=solution;
+    this.operation=operation;
+}
 
-    // Private attributes
-    #_x;
-    #_y;
-    #_solution;
-    #_operation;
-
-    // Setters and getters
-    get x() {
-      return this.#_x  
-    }
-    set x(value){
-        this.#_x = value;
-    }
-    get y() {
-        return this.#_y 
-    }
-    set y(value){
-          this.#_y = value;
-    }
-
-    get operation() {
-        return this.#_operation
-    }
-    set operation(value){
-          this.#_operation = value;
-    }
-
-    get solution() {
-        return this.#_solution
-    }
-    set solution(value){
-          this.#_solution = value;
-    }
 
     Calculer(){
-        this.#_solution = undefined;
-        switch (this.#_operation) {
+        this.solution = undefined;
+        switch (this.operation) {
             case '+':
-                this.#_solution = this.#_x + this.#_y;
+                this.solution = this.x + this.y;
                 break;
             case '-':
-                this. #_solution = this.#_x- this.#_y;
+                this. solution = this.x- this.y;
                     break;
             case '*':
-                this. #_solution = this.#_x* this.#_y;
+                this. solution = this.x* this.y;
                     break;
             case '/':
-                this. #_solution = this.#_x/ this.#_y;
+                this. solution = this.x/ this.y;
                     break;
             default:
                 break;
         }
-        return this.#_solution;
+        return this.solution;
     }
 
+
     Init(){
-        this.#_x = undefined;
-        this.#_y = undefined;
-        this.#_operation = undefined;
+        this.x = undefined;
+        this.y = undefined;
+        this.operation = undefined;
     }
 
   }
