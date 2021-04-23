5cdccc88be090e12884466c05f189fabd1d43ccd
dahrif <dahri.fatima.dev@gmail.com>
2021-04-23 11:07:54 +0000

    prototype2

--- prototype2/calc.php
+++ prototype2/calc.php
@@ -1,10 +1,12 @@
 <?php 
 function calculer($x,$y,$operation){
-    $solution=null;
+    $solution = null;
     switch($operation){
-        case'+': $solution = $x + $y;
+        case'+': 
+            $solution = $x + $y;
         break;
-        case '-': $solution = $x - $y;
+        case '-': 
+            $solution = $x - $y;
         break;
     }
     return $solution;
@@ -14,40 +16,51 @@ $x = null;
 $y = null;
 $operation = null;
 $solution = null;
-$affichage = '';
+$affichage = "";
 
-if(isset($_POST['x'])){
-    $x = $_POST['x'];}
-
-if(isset($_POST['y'])){
-    $y = $_POST['y'];}
-
-if(isset($_POST['operation'])){
-    $operation = $_POST['operation'];}
+if(isset($_POST['x'])) $x = $_POST['x'];
+if(isset($_POST['y']))  $y = $_POST['y'];
+if(isset($_POST['operation'])) $operation = $_POST['operation'];
 
 
 if(isset($_POST['number'])){
     $number =  $_POST['number'];
-    if($operation = null){
-        if($x = null){ $x = $number;}
-        else{$x = floatval($x . $number);}}
+    if($operation == null){
+        if($x == null)
+         $x = $number;
+        else $x = floatval($x . $number);
+    }else{
+        if($y == null) $y = $number;
+        else $y = floatval($y . $number);
+    }
 }
-else{
-    if($y = null){ $y = $number;}
-        else{$y = floatval($y . $number);}}
+        
 
 
 if (isset($_POST['egale'])){
     $egale = $_POST['egale'];
-}
 
 $solution = calculer($x,$y,$operation);
+}
 
 if($solution != null){
-    $affichage = $solution;
-} else{
-    
+     $affichage = $solution; 
+}else{
+     if($x != null) $affichage = $affichage . $x;
+     if($operation != null) $affichage .= '' . $operation .'';
+     if($y != null) $affichage = $affichage . $y;
 }
+
+if(isset($_POST['init'])){
+    $init = $_POST['init'];
+    $x = null;
+    $y = null;
+    $operation = null;
+    $solution = null;
+    $affichage = "";
+}
+
+
 
 
 
@@ -68,13 +81,14 @@ if($solution != null){
     <input type="hidden" name="x" value="<?php echo $x ?>">
     <input type="hidden" name="y" value="<?php echo $y ?>">
     <input type="hidden" name="operation" value="<?php echo $operation ?>">
-    <input type="text" name="affichage" value="<?php echo $affichage ?>">
-    <input type="button" name="number" value="1">
-    <input type="button" name="number" value="2">
-    <input type="button" name="number" value="3">
-    <input type="button" name="operation" value="+">
-    <input type="button" name="operation" value="-">
-    <input type="button" name="egale" value="=">
+    <input type="text" name="affichage" value="<?php echo $affichage ?>"><br>
+    <input type="submit" name="number" value="1">
+    <input type="submit" name="number" value="2">
+    <input type="submit" name="number" value="3">
+    <input type="submit" name="operation" value="+">
+    <input type="submit" name="operation" value="-">
+    <input type="submit" name="egale" value="=">
+    <input type="submit" name="init" value="C">
     </form>
 </body>
 </html>
